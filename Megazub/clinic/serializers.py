ebd3f3706412bac14b9da62c35392f921c0d46fd
Nazik <nazikmayram@gmail.com>
2024-03-11 14:29:01 +0600

    add doctors slider

--- Megazub/clinic/serializers.py
+++ Megazub/clinic/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import *
 
+
 class DoctorsListSerializer(serializers.ModelSerializer):
     """Список докторов"""
 
     class Meta:
         model = Doctors
+        fields = '__all__'
+
+
+class DoctorsSliderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DoctorsSlider
         fields = '__all__'
--- Megazub/clinic/views.py
+++ Megazub/clinic/views.py
@@ -1,9 +1,11 @@
-from .models import *
 from rest_framework import viewsets
-from .serializers import *
+from .serializers import DoctorsListSerializer, DoctorsSliderSerializer
+from .models import Doctors, DoctorsSlider
 
-class DoctorsListViewSets(viewsets):
+class DoctorsListViewSets(viewsets.ModelViewSet):
     queryset = Doctors.objects.all()
     serializer_class = DoctorsListSerializer
 
-
+class DoctorsSliderViewSets(viewsets.ModelViewSet):
+    queryset = DoctorsSlider.objects.all()
+    serializer_class = DoctorsSliderSerializer
